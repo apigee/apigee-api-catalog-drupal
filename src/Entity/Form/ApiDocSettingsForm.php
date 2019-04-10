028ec8606bc0689062d72aa825d6e18ea8a7c0fe
Arlina Espinoza <arlina.espinoza@gmail.com>
2019-04-10 12:03:39 -0700

    DRUP-624 Improving code from PR feedback.

--- src/Entity/Form/ApiDocSettingsForm.php
+++ src/Entity/Form/ApiDocSettingsForm.php
@@ -21,8 +21,10 @@
 namespace Drupal\apigee_api_catalog\Entity\Form;
 
 use Drupal\apigee_api_catalog\Entity\ApiDoc;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class ApiDocSettingsForm.
@@ -30,6 +32,29 @@ use Drupal\Core\Form\FormStateInterface;
  * Settings for the ApiDoc entity type.
  */
 class ApiDocSettingsForm extends FormBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -42,7 +67,7 @@ class ApiDocSettingsForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $entity_type = \Drupal::entityTypeManager()
+    $entity_type = $this->entityTypeManager
       ->getStorage('apidoc')
       ->getEntityType();
     $config = $this->configFactory()->getEditable('apigee_api_catalog.settings');
@@ -53,7 +78,7 @@ class ApiDocSettingsForm extends FormBase {
     $args = [
       '@type' => $entity_type->getLabel(),
     ];
-    drupal_set_message($this->t('@type settings have been updated.', $args));
+    $this->messenger($this->t('@type settings have been updated.', $args));
   }
 
   /**
@@ -68,7 +93,7 @@ class ApiDocSettingsForm extends FormBase {
 
     $form['workflow'] = [
       '#type' => 'details',
-      '#title' => t('Publishing options'),
+      '#title' => $this->t('Publishing options'),
       '#group' => 'additional_settings',
     ];
     $workflow_options = [
@@ -79,10 +104,10 @@ class ApiDocSettingsForm extends FormBase {
     $workflow_options = array_combine($keys, $keys);
     $form['workflow']['options'] = [
       '#type' => 'checkboxes',
-      '#title' => t('Default options'),
+      '#title' => $this->t('Default options'),
       '#default_value' => $workflow_options,
       '#options' => [
-        'new_revision' => t('Create new revision'),
+        'new_revision' => $this->t('Create new revision'),
       ],
     ];
 
