apigee/apigee-edge-drupal

DRUP-624 Add ApiDoc entity settings for default revision.

modules/apigee_edge_apidocs/src/Entity/ApiDoc.php
@@ -146,13 +146,6 @@ class ApiDoc extends EditorialContentEntityBase implements ApiDocInterface {
   /**
    * {@inheritdoc}
    */
-  public function save() {
-    return parent::save();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function preSave(EntityStorageInterface $storage) {
     parent::preSave($storage);
 
@@ -176,6 +169,18 @@ class ApiDoc extends EditorialContentEntityBase implements ApiDocInterface {
       // entry with an empty one.
       $record->revision_log_message = $this->original->revision_log_message->value;
     }
+  }
+
+  /**
+   * Gets whether a new revision should be created by default.
+   *
+   * @return bool
+   *   TRUE if a new revision should be created by default.
+   */
+  public static function shouldCreateNewRevision() {
+    $config = \Drupal::config('apigee_edge_apidocs.settings');
+    $default_revision = $config->get('default_revision');
+    return is_null($default_revision) ? TRUE : (bool) $default_revision;
   }
 
   /**

modules/apigee_edge_apidocs/src/Form/ApiDocSettingsForm.php
@@ -20,13 +20,14 @@
 
 namespace Drupal\apigee_edge_apidocs\Form;
 
+use Drupal\apigee_edge_apidocs\Entity\ApiDoc;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Class ApiDocSettingsForm.
  *
- * @todo Add settings or remove the class and the route.
+ * Settings for the ApiDoc entity type.
  */
 class ApiDocSettingsForm extends FormBase {
 
@@ -41,6 +42,18 @@ class ApiDocSettingsForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $entity_type = \Drupal::entityTypeManager()
+      ->getStorage('apidoc')
+      ->getEntityType();
+    $config = $this->configFactory()->getEditable('apigee_edge_apidocs.settings');
+
+    $options = $form_state->getValue('options');
+    $config->set('default_revision', (bool) $options['new_revision'])->save();
+
+    $args = [
+      '@type' => $entity_type->getLabel(),
+    ];
+    drupal_set_message($this->t('@type settings have been updated.', $args));
   }
 
   /**
@@ -48,6 +61,36 @@ class ApiDocSettingsForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['apidoc_settings']['#markup'] = $this->t('Settings for API Docs. Manage field settings using the tabs above.');
+
+    $form['additional_settings'] = [
+      '#type' => 'vertical_tabs',
+    ];
+
+    $form['workflow'] = [
+      '#type' => 'details',
+      '#title' => t('Publishing options'),
+      '#group' => 'additional_settings',
+    ];
+    $workflow_options = [
+      'new_revision' => ApiDoc::shouldCreateNewRevision(),
+    ];
+    // Prepare workflow options to be used for 'checkboxes' form element.
+    $keys = array_keys(array_filter($workflow_options));
+    $workflow_options = array_combine($keys, $keys);
+    $form['workflow']['options'] = [
+      '#type' => 'checkboxes',
+      '#title' => t('Default options'),
+      '#default_value' => $workflow_options,
+      '#options' => [
+        'new_revision' => t('Create new revision'),
+      ],
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+    ];
+
     return $form;
   }
 

apigee/apigee-api-catalog-drupal

src/Entity/ApiDoc.php
@@ -146,13 +146,6 @@ class ApiDoc extends EditorialContentEntityBase implements ApiDocInterface {
   /**
    * {@inheritdoc}
    */
-  public function save() {
-    return parent::save();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function preSave(EntityStorageInterface $storage) {
     parent::preSave($storage);
 
@@ -176,6 +169,18 @@ class ApiDoc extends EditorialContentEntityBase implements ApiDocInterface {
       // entry with an empty one.
       $record->revision_log_message = $this->original->revision_log_message->value;
     }
+  }
+
+  /**
+   * Gets whether a new revision should be created by default.
+   *
+   * @return bool
+   *   TRUE if a new revision should be created by default.
+   */
+  public static function shouldCreateNewRevision() {
+    $config = \Drupal::config('apigee_api_catalog.settings');
+    $default_revision = $config->get('default_revision');
+    return is_null($default_revision) ? TRUE : (bool) $default_revision;
   }
 
   /**

src/Entity/Form/ApiDocSettingsForm.php
@@ -20,13 +20,14 @@
 
 namespace Drupal\apigee_api_catalog\Entity\Form;
 
+use Drupal\apigee_api_catalog\Entity\ApiDoc;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Class ApiDocSettingsForm.
  *
- * @todo Add settings or remove the class and the route.
+ * Settings for the ApiDoc entity type.
  */
 class ApiDocSettingsForm extends FormBase {
 
@@ -41,6 +42,18 @@ class ApiDocSettingsForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $entity_type = \Drupal::entityTypeManager()
+      ->getStorage('apidoc')
+      ->getEntityType();
+    $config = $this->configFactory()->getEditable('apigee_api_catalog.settings');
+
+    $options = $form_state->getValue('options');
+    $config->set('default_revision', (bool) $options['new_revision'])->save();
+
+    $args = [
+      '@type' => $entity_type->getLabel(),
+    ];
+    drupal_set_message($this->t('@type settings have been updated.', $args));
   }
 
   /**
@@ -48,6 +61,36 @@ class ApiDocSettingsForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['apigee_api_catalog_settings']['#markup'] = $this->t('Settings for Apigee API catalog. Manage field settings using the tabs above.');
+
+    $form['additional_settings'] = [
+      '#type' => 'vertical_tabs',
+    ];
+
+    $form['workflow'] = [
+      '#type' => 'details',
+      '#title' => t('Publishing options'),
+      '#group' => 'additional_settings',
+    ];
+    $workflow_options = [
+      'new_revision' => ApiDoc::shouldCreateNewRevision(),
+    ];
+    // Prepare workflow options to be used for 'checkboxes' form element.
+    $keys = array_keys(array_filter($workflow_options));
+    $workflow_options = array_combine($keys, $keys);
+    $form['workflow']['options'] = [
+      '#type' => 'checkboxes',
+      '#title' => t('Default options'),
+      '#default_value' => $workflow_options,
+      '#options' => [
+        'new_revision' => t('Create new revision'),
+      ],
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+    ];
+
     return $form;
   }
 