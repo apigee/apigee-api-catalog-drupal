arlina-espinoza/apigee-edge-drupal

DRUP-459 Refactor API Doc open API spec fetcher as service.

modules/apigee_edge_apidocs/src/ApiDocSpecFetcher.php
@@ -1,0 +1,185 @@
+<?php
+
+/**
+ * Copyright 2019 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+namespace Drupal\apigee_edge_apidocs;
+
+use Drupal\apigee_edge_apidocs\Entity\ApiDocInterface;
+use Drupal\Core\Entity\EntityStorageException;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use GuzzleHttp\ClientInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class ApiDocSpecFetcher.
+ */
+class ApiDocSpecFetcher implements ApiDocSpecFetcherInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * Drupal\Core\File\FileSystemInterface definition.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  /**
+   * GuzzleHttp\ClientInterface definition.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Drupal\Core\Messenger\MessengerInterface definition.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * The logger.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  private $logger;
+
+  /**
+   * Constructs a new ApiDocSpecFetcher object.
+   *
+   * @param \Drupal\Core\File\FileSystemInterface $file_system
+   *   The file_system service.
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   The http_client service.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The logger service.
+   */
+  public function __construct(FileSystemInterface $file_system, ClientInterface $http_client, EntityTypeManagerInterface $entityTypeManager, MessengerInterface $messenger, LoggerInterface $logger) {
+    $this->fileSystem = $file_system;
+    $this->httpClient = $http_client;
+    $this->entityTypeManager = $entityTypeManager;
+    $this->messenger = $messenger;
+    $this->logger = $logger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fetchSpec(ApiDocInterface $apidoc, bool $save = TRUE, bool $new_revision = TRUE, bool $show_messages = TRUE) : bool {
+    $needs_save = FALSE;
+    $spec_value = $apidoc->get('spec')->isEmpty() ? [] : $apidoc->get('spec')->getValue()[0];
+
+    // If "spec_file_source" uses URL, grab file from "file_link" and save it
+    // into the "spec" file field. The file_link field should already have
+    // validated that a valid file exists at that URL.
+    if ($apidoc->get('spec_file_source')->value == ApiDocInterface::SPEC_AS_URL) {
+
+      // If the file_link field is empty, return without error.
+      if ($apidoc->get('file_link')->isEmpty()) {
+        return TRUE;
+      }
+
+      $file_uri = $apidoc->get('file_link')->getValue()[0]['uri'];
+      $data = file_get_contents($file_uri);
+      if (empty($data)) {
+        $message = 'Could not retrieve OpenAPI specification file located at %url';
+        $params = [
+          '%url' => $file_uri,
+        ];
+        $this->logger->error($message, $params);
+        if ($show_messages) {
+          $this->messenger->addMessage($this->t($message, $params), 'error');
+        }
+        return FALSE;
+      }
+
+      // Only save file if it hasn't been fetched previously.
+      $data_md5 = md5($data);
+      $prev_md5 = $apidoc->get('spec_md5')->isEmpty() ? NULL : $apidoc->get('spec_md5')->value;
+      if ($prev_md5 != $data_md5) {
+        $filename = $this->fileSystem->basename($file_uri);
+        $specs_definition = $apidoc->getFieldDefinition('spec')->getItemDefinition();
+        $target_dir = $specs_definition->getSetting('file_directory');
+        $uri_scheme = $specs_definition->getSetting('uri_scheme');
+        $file = file_save_data($data, "$uri_scheme://$target_dir/$filename", FILE_EXISTS_RENAME);
+        $spec_value = [
+            'target_id' => $file->id(),
+          ] + $spec_value;
+        $apidoc->set('spec', $spec_value);
+        $apidoc->set('spec_md5', $data_md5);
+
+        $needs_save = TRUE;
+      }
+    }
+
+    elseif (!empty($spec_value['target_id'])) {
+      /* @var \Drupal\file\Entity\File $file */
+      $file = $this->entityTypeManager
+        ->getStorage('file')
+        ->load($spec_value['target_id']);
+
+      if ($file) {
+        $prev_md5 = $apidoc->get('spec_md5')->isEmpty() ? NULL : $apidoc->get('spec_md5')->value;
+        $file_md5 = md5_file($file->getFileUri());
+        if ($prev_md5 != $file_md5) {
+          $apidoc->set('spec_md5', $file_md5);
+          $needs_save = TRUE;
+        }
+      }
+    }
+
+    // Only save if changes were made.
+    if ($save && $needs_save) {
+      if ($new_revision && $apidoc->getEntityType()->isRevisionable()) {
+        $apidoc->setNewRevision();
+      }
+
+      try {
+        $apidoc->save();
+      }
+      catch (EntityStorageException $e) {
+        $message = 'Error while saving API Doc while fetching OpenAPI specification file located at %url';
+        $params = [
+          '%url' => $file_uri,
+        ];
+        $this->logger->error($message, $params);
+        if ($show_messages) {
+          $this->messenger->addMessage($this->t($message, $params), 'error');
+        }
+        return FALSE;
+      }
+    }
+
+    return TRUE;
+  }
+
+}

modules/apigee_edge_apidocs/src/ApiDocSpecFetcherInterface.php
@@ -1,0 +1,51 @@
+<?php
+
+/**
+ * Copyright 2019 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+namespace Drupal\apigee_edge_apidocs;
+
+use Drupal\apigee_edge_apidocs\Entity\ApiDocInterface;
+
+/**
+ * Interface ApiDocSpecFetcherInterface.
+ */
+interface ApiDocSpecFetcherInterface {
+
+  /**
+   * Fetch OpenAPI specification file from URL.
+   *
+   * Takes care of updating an ApiDoc entity with the updated spec file. If
+   * "spec_file_source" uses a URL, it will fetch the specified file and put it
+   * in the "spec" file field. If it uses a "file", it won't change it.
+   *
+   * @param \Drupal\apigee_edge_apidocs\Entity\ApiDocInterface $apidoc
+   *   The ApiDoc entity.
+   * @param bool $save
+   *   Boolean indicating if method should save the entity.
+   * @param bool $new_revision
+   *   Boolean indicating if method should create a new revision when saving
+   *   the entity.
+   * @param bool $show_messages
+   *   Boolean indicating if method should display status messages.
+   *
+   * @return bool
+   *   Returns TRUE if the operation completed without errors.
+   */
+  public function fetchSpec(ApiDocInterface $apidoc, bool $save = TRUE, bool $new_revision = TRUE, bool $show_messages = TRUE) : bool;
+}

modules/apigee_edge_apidocs/src/Entity/ApiDoc.php
@@ -287,80 +287,7 @@ class ApiDoc extends ContentEntityBase implements ApiDocInterface {
   public function preSave(EntityStorageInterface $storage) {
     parent::preSave($storage);
 
-    $this->reimportOpenApiSpecFile(FALSE, FALSE);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function reimportOpenApiSpecFile(bool $save = TRUE, bool $new_revision = TRUE) {
-    $needs_save = FALSE;
-    $spec_value = $this->get('spec')->isEmpty() ? [] : $this->get('spec')->getValue()[0];
-
-    // If "spec_file_source" uses URL, grab file from "file_link" and save it
-    // into the "spec" file field. The file_link field should already have
-    // validated that a valid file exists at that URL.
-    if ($this->get('spec_file_source')->value == ApiDocInterface::SPEC_AS_URL) {
-
-      // If the file_link field is empty, return without showing errors.
-      if ($this->get('file_link')->isEmpty()) {
-        return TRUE;
-      }
-
-      $file_uri = $this->get('file_link')->getValue()[0]['uri'];
-      $data = file_get_contents($file_uri);
-      if (empty($data)) {
-        \Drupal::messenger()
-          ->addMessage($this->t('Could not retrieve OpenAPI specifications file located at %url', [
-            '%url' => $file_uri,
-          ]), 'error');
-        return FALSE;
-      }
-
-      // Only save file if it hasn't been fetched previously.
-      $data_md5 = md5($data);
-      $prev_md5 = $this->get('spec_md5')->isEmpty() ? NULL : $this->get('spec_md5')->value;
-      if ($prev_md5 != $data_md5) {
-        $filename = \Drupal::service('file_system')->basename($file_uri);
-        $specs_definition = $this->getFieldDefinition('spec')->getItemDefinition();
-        $target_dir = $specs_definition->getSetting('file_directory');
-        $uri_scheme = $specs_definition->getSetting('uri_scheme');
-        $file = file_save_data($data, "$uri_scheme://$target_dir/$filename", FILE_EXISTS_RENAME);
-        $spec_value = [
-          'target_id' => $file->id(),
-        ] + $spec_value;
-        $this->set('spec', $spec_value);
-        $this->set('spec_md5', $data_md5);
-
-        $needs_save = TRUE;
-      }
-    }
-
-    elseif (!empty($spec_value['target_id'])) {
-      /* @var \Drupal\file\Entity\File $file */
-      $file = \Drupal::entityTypeManager()
-        ->getStorage('file')
-        ->load($spec_value['target_id']);
-
-      if ($file) {
-        $prev_md5 = $this->get('spec_md5')->isEmpty() ? NULL : $this->get('spec_md5')->value;
-        $file_md5 = md5_file($file->getFileUri());
-        if ($prev_md5 != $file_md5) {
-          $this->set('spec_md5', $file_md5);
-          $needs_save = TRUE;
-        }
-      }
-    }
-
-    // Only save if changes were made.
-    if ($save && $needs_save) {
-      if ($new_revision && $this->getEntityType()->isRevisionable()) {
-        $this->setNewRevision();
-      }
-      $this->save();
-    }
-
-    return TRUE;
+    \Drupal::service('apigee_edge_apidocs.spec_fetcher')->fetchSpec($this, FALSE, FALSE);
   }
 
 }

modules/apigee_edge_apidocs/src/Form/ApiDocUpdateSpecForm.php
@@ -2,22 +2,71 @@
 
 namespace Drupal\apigee_edge_apidocs\Form;
 
+use Drupal\apigee_edge_apidocs\ApiDocSpecFetcherInterface;
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\ContentEntityConfirmFormBase;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Messenger\MessengerTrait;
+use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class ApiDocUpdateSpecForm.
  */
 class ApiDocUpdateSpecForm extends ContentEntityConfirmFormBase {
 
-  use MessengerTrait;
+  /**
+   * The messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * The ApiDoc spec fetcher service.
+   *
+   * @var \Drupal\apigee_edge_apidocs\ApiDocSpecFetcherInterface
+   */
+  protected $specFetcher;
 
   /**
    * {@inheritdoc}
    */
   protected $operation = 'reimport_spec';
+
+  /**
+   * Constructs a ApiDocUpdateSpecForm object.
+   *
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository service.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
+   *   The entity type bundle service.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The time service.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   * @param \Drupal\apigee_edge_apidocs\ApiDocSpecFetcherInterface $spec_fetcher
+   *   The ApiDoc spec fetcher service.
+   */
+  public function __construct(EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL, TimeInterface $time = NULL, MessengerInterface $messenger, ApiDocSpecFetcherInterface $spec_fetcher) {
+    parent::__construct($entity_repository, $entity_type_bundle_info, $time);
+    $this->messenger = $messenger;
+    $this->specFetcher = $spec_fetcher;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity.repository'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('datetime.time'),
+      $container->get('messenger'),
+      $container->get('apigee_edge_apidocs.spec_fetcher')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -56,10 +105,10 @@ class ApiDocUpdateSpecForm extends ContentEntityConfirmFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     /* @var \Drupal\apigee_edge_apidocs\Entity\ApiDocInterface $entity */
     $entity = $this->getEntity();
-    $status = $entity->reimportOpenApiSpecFile(TRUE, TRUE);
+    $status = $this->specFetcher->fetchSpec($entity, FALSE, FALSE);
 
     if ($status) {
-      $this->messenger()->addStatus($this->t('API Doc %label: imported the OpenAPI
+      $this->messenger->addStatus($this->t('API Doc %label: imported the OpenAPI
       specification file from URL.', [
         '%label' => $this->entity->label(),
       ]));

apigee/apigee-api-catalog-drupal

src/ApiDocSpecFetcher.php
@@ -1,0 +1,185 @@
+<?php
+
+/**
+ * Copyright 2019 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+namespace Drupal\apigee_api_catalog;
+
+use Drupal\apigee_api_catalog\Entity\ApiDocInterface;
+use Drupal\Core\Entity\EntityStorageException;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use GuzzleHttp\ClientInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class ApiDocSpecFetcher.
+ */
+class ApiDocSpecFetcher implements ApiDocSpecFetcherInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * Drupal\Core\File\FileSystemInterface definition.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  /**
+   * GuzzleHttp\ClientInterface definition.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Drupal\Core\Messenger\MessengerInterface definition.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * The logger.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  private $logger;
+
+  /**
+   * Constructs a new ApiDocSpecFetcher object.
+   *
+   * @param \Drupal\Core\File\FileSystemInterface $file_system
+   *   The file_system service.
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   The http_client service.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The logger service.
+   */
+  public function __construct(FileSystemInterface $file_system, ClientInterface $http_client, EntityTypeManagerInterface $entityTypeManager, MessengerInterface $messenger, LoggerInterface $logger) {
+    $this->fileSystem = $file_system;
+    $this->httpClient = $http_client;
+    $this->entityTypeManager = $entityTypeManager;
+    $this->messenger = $messenger;
+    $this->logger = $logger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fetchSpec(ApiDocInterface $apidoc, bool $save = TRUE, bool $new_revision = TRUE, bool $show_messages = TRUE) : bool {
+    $needs_save = FALSE;
+    $spec_value = $apidoc->get('spec')->isEmpty() ? [] : $apidoc->get('spec')->getValue()[0];
+
+    // If "spec_file_source" uses URL, grab file from "file_link" and save it
+    // into the "spec" file field. The file_link field should already have
+    // validated that a valid file exists at that URL.
+    if ($apidoc->get('spec_file_source')->value == ApiDocInterface::SPEC_AS_URL) {
+
+      // If the file_link field is empty, return without error.
+      if ($apidoc->get('file_link')->isEmpty()) {
+        return TRUE;
+      }
+
+      $file_uri = $apidoc->get('file_link')->getValue()[0]['uri'];
+      $data = file_get_contents($file_uri);
+      if (empty($data)) {
+        $message = 'Could not retrieve OpenAPI specification file located at %url';
+        $params = [
+          '%url' => $file_uri,
+        ];
+        $this->logger->error($message, $params);
+        if ($show_messages) {
+          $this->messenger->addMessage($this->t($message, $params), 'error');
+        }
+        return FALSE;
+      }
+
+      // Only save file if it hasn't been fetched previously.
+      $data_md5 = md5($data);
+      $prev_md5 = $apidoc->get('spec_md5')->isEmpty() ? NULL : $apidoc->get('spec_md5')->value;
+      if ($prev_md5 != $data_md5) {
+        $filename = $this->fileSystem->basename($file_uri);
+        $specs_definition = $apidoc->getFieldDefinition('spec')->getItemDefinition();
+        $target_dir = $specs_definition->getSetting('file_directory');
+        $uri_scheme = $specs_definition->getSetting('uri_scheme');
+        $file = file_save_data($data, "$uri_scheme://$target_dir/$filename", FILE_EXISTS_RENAME);
+        $spec_value = [
+            'target_id' => $file->id(),
+          ] + $spec_value;
+        $apidoc->set('spec', $spec_value);
+        $apidoc->set('spec_md5', $data_md5);
+
+        $needs_save = TRUE;
+      }
+    }
+
+    elseif (!empty($spec_value['target_id'])) {
+      /* @var \Drupal\file\Entity\File $file */
+      $file = $this->entityTypeManager
+        ->getStorage('file')
+        ->load($spec_value['target_id']);
+
+      if ($file) {
+        $prev_md5 = $apidoc->get('spec_md5')->isEmpty() ? NULL : $apidoc->get('spec_md5')->value;
+        $file_md5 = md5_file($file->getFileUri());
+        if ($prev_md5 != $file_md5) {
+          $apidoc->set('spec_md5', $file_md5);
+          $needs_save = TRUE;
+        }
+      }
+    }
+
+    // Only save if changes were made.
+    if ($save && $needs_save) {
+      if ($new_revision && $apidoc->getEntityType()->isRevisionable()) {
+        $apidoc->setNewRevision();
+      }
+
+      try {
+        $apidoc->save();
+      }
+      catch (EntityStorageException $e) {
+        $message = 'Error while saving API Doc while fetching OpenAPI specification file located at %url';
+        $params = [
+          '%url' => $file_uri,
+        ];
+        $this->logger->error($message, $params);
+        if ($show_messages) {
+          $this->messenger->addMessage($this->t($message, $params), 'error');
+        }
+        return FALSE;
+      }
+    }
+
+    return TRUE;
+  }
+
+}

src/ApiDocSpecFetcherInterface.php
@@ -1,0 +1,51 @@
+<?php
+
+/**
+ * Copyright 2019 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+namespace Drupal\apigee_api_catalog;
+
+use Drupal\apigee_api_catalog\Entity\ApiDocInterface;
+
+/**
+ * Interface ApiDocSpecFetcherInterface.
+ */
+interface ApiDocSpecFetcherInterface {
+
+  /**
+   * Fetch OpenAPI specification file from URL.
+   *
+   * Takes care of updating an ApiDoc entity with the updated spec file. If
+   * "spec_file_source" uses a URL, it will fetch the specified file and put it
+   * in the "spec" file field. If it uses a "file", it won't change it.
+   *
+   * @param \Drupal\apigee_api_catalog\Entity\ApiDocInterface $apidoc
+   *   The ApiDoc entity.
+   * @param bool $save
+   *   Boolean indicating if method should save the entity.
+   * @param bool $new_revision
+   *   Boolean indicating if method should create a new revision when saving
+   *   the entity.
+   * @param bool $show_messages
+   *   Boolean indicating if method should display status messages.
+   *
+   * @return bool
+   *   Returns TRUE if the operation completed without errors.
+   */
+  public function fetchSpec(ApiDocInterface $apidoc, bool $save = TRUE, bool $new_revision = TRUE, bool $show_messages = TRUE) : bool;
+}

src/Entity/ApiDoc.php
@@ -284,80 +284,7 @@ class ApiDoc extends ContentEntityBase implements ApiDocInterface {
   public function preSave(EntityStorageInterface $storage) {
     parent::preSave($storage);
 
-    $this->reimportOpenApiSpecFile(FALSE, FALSE);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function reimportOpenApiSpecFile(bool $save = TRUE, bool $new_revision = TRUE) {
-    $needs_save = FALSE;
-    $spec_value = $this->get('spec')->isEmpty() ? [] : $this->get('spec')->getValue()[0];
-
-    // If "spec_file_source" uses URL, grab file from "file_link" and save it
-    // into the "spec" file field. The file_link field should already have
-    // validated that a valid file exists at that URL.
-    if ($this->get('spec_file_source')->value == ApiDocInterface::SPEC_AS_URL) {
-
-      // If the file_link field is empty, return without showing errors.
-      if ($this->get('file_link')->isEmpty()) {
-        return TRUE;
-      }
-
-      $file_uri = $this->get('file_link')->getValue()[0]['uri'];
-      $data = file_get_contents($file_uri);
-      if (empty($data)) {
-        \Drupal::messenger()
-          ->addMessage($this->t('Could not retrieve OpenAPI specifications file located at %url', [
-            '%url' => $file_uri,
-          ]), 'error');
-        return FALSE;
-      }
-
-      // Only save file if it hasn't been fetched previously.
-      $data_md5 = md5($data);
-      $prev_md5 = $this->get('spec_md5')->isEmpty() ? NULL : $this->get('spec_md5')->value;
-      if ($prev_md5 != $data_md5) {
-        $filename = \Drupal::service('file_system')->basename($file_uri);
-        $specs_definition = $this->getFieldDefinition('spec')->getItemDefinition();
-        $target_dir = $specs_definition->getSetting('file_directory');
-        $uri_scheme = $specs_definition->getSetting('uri_scheme');
-        $file = file_save_data($data, "$uri_scheme://$target_dir/$filename", FILE_EXISTS_RENAME);
-        $spec_value = [
-          'target_id' => $file->id(),
-        ] + $spec_value;
-        $this->set('spec', $spec_value);
-        $this->set('spec_md5', $data_md5);
-
-        $needs_save = TRUE;
-      }
-    }
-
-    elseif (!empty($spec_value['target_id'])) {
-      /* @var \Drupal\file\Entity\File $file */
-      $file = \Drupal::entityTypeManager()
-        ->getStorage('file')
-        ->load($spec_value['target_id']);
-
-      if ($file) {
-        $prev_md5 = $this->get('spec_md5')->isEmpty() ? NULL : $this->get('spec_md5')->value;
-        $file_md5 = md5_file($file->getFileUri());
-        if ($prev_md5 != $file_md5) {
-          $this->set('spec_md5', $file_md5);
-          $needs_save = TRUE;
-        }
-      }
-    }
-
-    // Only save if changes were made.
-    if ($save && $needs_save) {
-      if ($new_revision && $this->getEntityType()->isRevisionable()) {
-        $this->setNewRevision();
-      }
-      $this->save();
-    }
-
-    return TRUE;
+    \Drupal::service('apigee_api_catalog.spec_fetcher')->fetchSpec($this, FALSE, FALSE);
   }
 
 }

src/Form/ApiDocUpdateSpecForm.php
@@ -2,22 +2,71 @@
 
 namespace Drupal\apigee_api_catalog\Form;
 
+use Drupal\apigee_api_catalog\ApiDocSpecFetcherInterface;
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\ContentEntityConfirmFormBase;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Messenger\MessengerTrait;
+use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class ApiDocUpdateSpecForm.
  */
 class ApiDocUpdateSpecForm extends ContentEntityConfirmFormBase {
 
-  use MessengerTrait;
+  /**
+   * The messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * The ApiDoc spec fetcher service.
+   *
+   * @var \Drupal\apigee_api_catalog\ApiDocSpecFetcherInterface
+   */
+  protected $specFetcher;
 
   /**
    * {@inheritdoc}
    */
   protected $operation = 'reimport_spec';
+
+  /**
+   * Constructs a ApiDocUpdateSpecForm object.
+   *
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository service.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
+   *   The entity type bundle service.
+   * @param \Drupal\Component\Datetime\TimeInterface $time
+   *   The time service.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   * @param \Drupal\apigee_api_catalog\ApiDocSpecFetcherInterface $spec_fetcher
+   *   The ApiDoc spec fetcher service.
+   */
+  public function __construct(EntityRepositoryInterface $entity_repository, EntityTypeBundleInfoInterface $entity_type_bundle_info = NULL, TimeInterface $time = NULL, MessengerInterface $messenger, ApiDocSpecFetcherInterface $spec_fetcher) {
+    parent::__construct($entity_repository, $entity_type_bundle_info, $time);
+    $this->messenger = $messenger;
+    $this->specFetcher = $spec_fetcher;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity.repository'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('datetime.time'),
+      $container->get('messenger'),
+      $container->get('apigee_api_catalog.spec_fetcher')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -56,10 +105,10 @@ class ApiDocUpdateSpecForm extends ContentEntityConfirmFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     /* @var \Drupal\apigee_api_catalog\Entity\ApiDocInterface $entity */
     $entity = $this->getEntity();
-    $status = $entity->reimportOpenApiSpecFile(TRUE, TRUE);
+    $status = $this->specFetcher->fetchSpec($entity, FALSE, FALSE);
 
     if ($status) {
-      $this->messenger()->addStatus($this->t('API Doc %label: imported the OpenAPI
+      $this->messenger->addStatus($this->t('API Doc %label: imported the OpenAPI
       specification file from URL.', [
         '%label' => $this->entity->label(),
       ]));