df17a7f16c96542f17609ba50d5d8cc690ea0be9
Arlina Espinoza <arlina.espinoza@gmail.com>
2020-09-25 16:27:46 -0700

    [#102] Add circleCI testing.

---
+++ .circleci/update-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ex
+
+# Make sure the robofile is in the correct location.
+cp modules/apigee_api_catalog/.circleci/RoboFile.php ./
+
+robo setup:skeleton
+robo add:modules $1
+robo drupal:version $2
+robo configure:module-dependencies
+robo update:dependencies
+robo do:extra $2
+
+# Touch a flag so we know dependencies have been set. Otherwise, there is no
+# easy way to know this step needs to be done when running circleci locally since
+# it does not support workflows.
+touch dependencies_updated
